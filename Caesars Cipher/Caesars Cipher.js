34c532673a13f6b73f402343a434d1982d2a818f
Jimena <jimenaa971@gmail.com>
2022-04-05 13:55:11 +0200

    improve Caesars Cipher script

--- Caesars Cipher/Caesars Cipher.js
+++ Caesars Cipher/Caesars Cipher.js
@@ -5,19 +5,13 @@ function decipher(chr){
   if( index == -1){
     return chr;
   } else {
-      const decipherIndex = index + 13;
-      if(decipherIndex >= 26){
-        return alphabet.charAt(decipherIndex - 26);
-      } else {
-        return alphabet.charAt(decipherIndex);
-      }
+    const decipherIndex = (index + 13) % 26;
+    return alphabet.charAt(decipherIndex);
   }
 }
   
 function rot13(str) {
-  const decipheredArr = [];
-  
-  str.split( '' ).map( chr => decipheredArr.push(decipher(chr)) );
+  const decipheredArr = str.split('').map(decipher);
   
   return decipheredArr.join('');
 }
