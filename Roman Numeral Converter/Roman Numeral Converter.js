fb0f251bc0a5cedd7ee470e94dd865dee56c8386
Jimena <jimenaa971@gmail.com>
2022-04-06 14:04:46 +0200

    improve Roman Numeral Converter script

--- Roman Numeral Converter/Roman Numeral Converter.js
+++ Roman Numeral Converter/Roman Numeral Converter.js
@@ -8,7 +8,7 @@ const romanSymbols = {
   1000: 'M'
 };
 
-//Divides the number into ones, tens, hundreds and thousands
+// Divides the number into ones, tens, hundreds and thousands
 function splitNum (num){
 	var numArr = num.toString().split('');
 
@@ -21,76 +21,36 @@ function splitNum (num){
 
 function convertToRoman(num) {
 	let splitNumArr = splitNum(num);
+
 	let ones = splitNumArr[3];
-	let tens = splitNumArr[2] * 10;
-	let hundreds = splitNumArr[1] * 100;
-	let thousands = splitNumArr[0] * 1000;
-	let romanNum = ''.concat(convertThousands(thousands), convertHundreds(hundreds), convertTens(tens), convertOnes(ones));
+	let tens = splitNumArr[2];
+	let hundreds = splitNumArr[1];
+	let thousands = splitNumArr[0];
+
+  let converteredOnes = convert(ones, romanSymbols[1], romanSymbols[5], romanSymbols[10]);
+  let converteredTens = convert(tens, romanSymbols[10], romanSymbols[50], romanSymbols[100]);
+  let converteredHundreds = convert(hundreds, romanSymbols[100], romanSymbols[500], romanSymbols[1000] );
+  let converteredThousands = convertThousands(thousands);
+
+	let romanNum = ''.concat(converteredThousands, converteredHundreds, converteredTens, converteredOnes);
 
 	return romanNum;
 }
 
-//Functions that convert each part of the number in roman numeral.
-function convertOnes(num){
-	if (num == 0) {
-		return '';
-	} else if (num == 1){
-		  return romanSymbols[1];
-	} else if ( num == 5 ){
-		  return romanSymbols[5];
-	} else if ( num == 4){
-			return ''.concat(romanSymbols[1], romanSymbols[5])
-	} else if ( num == 9){
-			return ''.concat(romanSymbols[1], romanSymbols[10])
-	} else if ( num < 4){
-			return ''.concat(romanSymbols[1].repeat(num));
-	} else if ( num < 9){
-			return ''.concat(romanSymbols[5], romanSymbols[1].repeat(num - 5));
-	}
-}
-
-function convertTens(num){
-	if (num == 0) {
-		return '';
-	} else if (num == 10){
-		  return romanSymbols[10];
-	} else if ( num == 50 ){
-			return romanSymbols[50];
-	} else if ( num == 40){
-			return ''.concat(romanSymbols[10], romanSymbols[50])
-	} else if ( num == 90){
-			return ''.concat(romanSymbols[10], romanSymbols[100])
-	} else if ( num < 40){
-			return ''.concat(romanSymbols[10].repeat(Math.trunc( num / 10 )));
-	} else if ( num < 90){
-			return ''.concat(romanSymbols[50], romanSymbols[10].repeat(Math.trunc( num / 10 ) - 5));
-	}
-}
-
-function convertHundreds(num){
-	if (num == 0) {
-		return '';
-	} else if (num == 100){
-		  return romanSymbols[100];
-	} else if ( num == 500 ){
-			return romanSymbols[500];
-	} else if ( num == 400 ){
-			return ''.concat(romanSymbols[100], romanSymbols[500])
-	} else if ( num == 900){
-			return ''.concat(romanSymbols[100], romanSymbols[1000])
-	} else if ( num < 400){
-			return ''.concat(romanSymbols[100].repeat(Math.trunc( num / 100 )));
-	} else if ( num < 900){
-			return ''.concat(romanSymbols[500], romanSymbols[100].repeat(Math.trunc( num / 100 ) - 5));
-	}
-}
-  
-function convertThousands(num){
-  if (num == 0) {
-    return '';
-  } else {
-      return ''.concat(romanSymbols[1000].repeat(num / 1000));
+function convert (digit, unitSymbol, fiveSymbol, tenSymbol) {
+	if ( digit == 4 ) {
+		return ''.concat(unitSymbol, fiveSymbol)
+  } else if ( digit == 9 ) {
+    return ''.concat(unitSymbol, tenSymbol)
+  } else if ( digit < 4 ) {
+    return ''.concat(unitSymbol.repeat(digit));
+  } else if ( digit < 9 ) {
+    return ''.concat(fiveSymbol, unitSymbol.repeat(digit - 5));
   }
+}
+
+function convertThousands(num) {
+  return ''.concat(romanSymbols[1000].repeat(num));
 }
 
 export default convertToRoman;
